a161e13cdb1528cc65e4909f2ab5e17c27d77ff5
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2023-12-30 09:14:54 +0100

    add some structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='daiky',
+    version='0.0.1',
+    description='.',
+    long_description=readme,
+    author='Filip Swiszcz'
+)
